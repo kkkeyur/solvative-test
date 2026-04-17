44b2116cd3b139829ea2be5d68ca30f788b8c9c5
kkkeyur <keyurchotaliyakk@gmail.com>
2026-04-17 14:16:27 +0530

    initial commit

---
+++ src/test/java/com/solvative/book_vault/BookVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvative.book_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
